08b3275fba0bc01a0c21aacc5a9881d5aedb2e69
k-sucharitha <kakanurusucharitha@gmail.com>
2022-12-20 11:16:04 +0530

    added form validation.

--- meeting_planner/urls.py
+++ meeting_planner/urls.py
@@ -19,7 +19,7 @@ from website.views import welcome, date, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',welcome, name='welcome'),
+    path('', welcome, name='welcome'),
     path('date/', date),
     path('about/', about),
     path('meetings/',include('meetings.urls')),
